423a8fc902cb1ee0b86ad4e4db8f3349425424a0
LyonsAus <alexlyons12@gmail.com>
2017-09-18 12:45:22 +1000

    update

--- Menu Test/studentlist.c
+++ Menu Test/studentlist.c
@@ -10,12 +10,12 @@ A brief statement on what you could achieve (less than 50 words):
 - Filtering invalid input and responding
 - Adding Students with a maximum class size
 - Deleting last student
-- Storing Student list in DB 
+- Storing Student list in DB
 - Displaying student list
 
 A brief statement on what you could NOT achieve (less than 50 words):
 
-- Print the name with spaces 
+- Print the name with spaces
 
 
 *******************************************************************************/
@@ -35,7 +35,7 @@ List preprocessing directives - you may define your own.
 *******************************************************************************/
 /*Max students in a class*/
 #define MAX_CLASS_SIZE 5
-#define MAX_NAME_SIZE 11 
+#define MAX_NAME_SIZE 11
 #define DB_FILE_NAME "database"
 
 
@@ -48,7 +48,7 @@ struct student {
 		}birthday;
 	/* Declaration of Char Name*/
 	char name[MAX_NAME_SIZE];
-       /*Declaration of float GPA */ 
+       /*Declaration of float GPA */
 	float gpa;
 
 };
@@ -60,10 +60,10 @@ int IsMonthValid(int storMonth);
 int IsYearValid(int storYear);
 int sign_In(char user[30], char pass[30]);
 /*******************************************************************************
-Main - Contains Menu with appropriate functions 
+Main - Contains Menu with appropriate functions
 *******************************************************************************/
 int main(void) {
-	
+
 
 
 char user[30], pass[30];
@@ -73,7 +73,7 @@ char user[30], pass[30];
     printf("\nPassword:");
     scanf("%s",pass);
 
-    sign_In(user, pass);     
+    sign_In(user, pass);
 return 0;
 }
 
@@ -84,7 +84,7 @@ program.
 Inputs:
 - Name
 - Date of birth (day, month, year)
-- GPA 
+- GPA
 Outputs:
 - Add student to the list
 - Delete last student in the list
@@ -130,14 +130,14 @@ int IsDayValid(int storDay) 	{
 int IsMonthValid(int storMonth) 	{
 	if (storMonth<1)	{
 		printf("Invalid month. \n");
-		return 0; 
+		return 0;
 	}
 	else if(storMonth>12)	{
 		printf("Invalid month. \n");
-		return 0; 
+		return 0;
 	}
 	else	{
-	        return 1; 
+	        return 1;
 	}
 }
 
@@ -145,14 +145,14 @@ int IsMonthValid(int storMonth) 	{
 int IsYearValid(int storYear)	{
 	if (storYear<1800)	{
 		printf("Invalid year. \n");
-	        return 0; 
+	        return 0;
 	}
 	else if(storYear>2017)	{
 		printf("Invalid year. \n");
-	        return 0; 
+	        return 0;
 	}
 	else	{
-	        return 1; 
+	        return 1;
 	}
 }
 
@@ -165,7 +165,7 @@ int sign_In(char user[30], char pass[30]){
 	FILE * fPointer;
 	int arraycount=0;
 	int count;
-	struct student studentlist[MAX_CLASS_SIZE];    
+	struct student studentlist[MAX_CLASS_SIZE];
 FILE *p;
     char user2[30], pass2[30];
 
@@ -176,53 +176,53 @@ FILE *p;
 
     if( (strcmp(user,user2)==0) && (strcmp(pass,pass2)==0) ){
        printMenu();
-       
+
 do {
-    
+
 
     /*Captures input for menu selection*/
     scanf("%d",&Menuchoice);
 
 
 	switch(Menuchoice)	{
-     	    	 
+
 		case 1:		{
 			if (arraycount<MAX_CLASS_SIZE)	  {
-					 
+
                 printf("Enter name>");
 				scanf("%s",studentlist[arraycount].name);
-					
 
-			day: 
-		         printf("Enter birthday: day>");   
+
+			day:
+		         printf("Enter birthday: day>");
 		         char dy[100];
-		         
-            	 scanf("%s", dy); 			
-            	 	 
-            	 	 int a=atoi(dy); 		
+
+            	 scanf("%s", dy);
+
+            	 	 int a=atoi(dy);
             	     if (IsDayValid(a)==0)	{
                 	             goto day;
             	     }
 
             	     studentlist[arraycount].birthday.day=a;
-				
-				
-	        month: 
+
+
+	        month:
 
 		          printf("Enter birthday: month>");
 		          char mon[100];
-            	
-		          scanf("%s",mon);		  	  
-		          	  int b=atoi(mon);	   
+
+		          scanf("%s",mon);
+		          	  int b=atoi(mon);
 		          	  if(IsMonthValid(b)==0)	{
 		          		  	  	 goto month;
 		          	  }
 
             	      studentlist[arraycount].birthday.month=b;
-			   
 
-	          year: 
-		
+
+	          year:
+
                    printf("Enter birthday: year>");
                    char yr[100];
 
@@ -231,14 +231,14 @@ do {
             	       if(IsYearValid(c)==0)	{
             	    	   	   	   goto year;
             	       }
-            	       
+
             	       studentlist[arraycount].birthday.year=c;
-	    
-	           gpa: 
-               
+
+	           gpa:
+
 			       printf("Enter GPA>");
 			       char g[1000];
-			
+
                    scanf("%s", g);
                    	  float e= atof(g);/*GPA string to Floating point (Double)*/
 
@@ -246,47 +246,47 @@ do {
 			          printf("Invalid GPA. \n");
 			          	  	  	goto gpa;
 			        }
-			       
+
                    	studentlist[arraycount].gpa=e;
-                              
+
                    	arraycount++;
-  
+
 		}
-                        
-		else	{     
+
+		else	{
 
 			 printf("Class is full \n");
-			 } 
+			 }
 				break;
-		}		
-     	    
+		}
+
 
 		case 2:		{   /* Wont delete if class is empty */
 				if (arraycount==0)	{
 					printf("Class is empty\n");
 				}
-				else if(arraycount>0)	{  
-                    /* Delete last line of the list */ 
+				else if(arraycount>0)	{
+                    /* Delete last line of the list */
 				    studentlist[arraycount-1]=studentlist[arraycount];
-				    
-                    /* List of student is decrease from bottom */ 
-				    arraycount--;				
+
+                    /* List of student is decrease from bottom */
+				    arraycount--;
 			}
 		    	 break;
 		}
-     	    	
+
      	 case 3:	{
      		 	 	 /* Check if class has students */
 		    	    if(arraycount==0)	{
 		    		    printf("Class is empty\n");
 				  }
-				
+
 				else	{
 				        printf("Name       Birthday   GPA   \n");
 			    	    printf("---------- ---------- ------\n");
-			    	
+
 			/*displays the studentlist until the end of it */
-					
+
 			    for (count=0;count<arraycount;count++) {
 			    		printf("%-10.10s",studentlist[count].name);
                         printf(" %02d",studentlist[count].birthday.day);
@@ -294,43 +294,43 @@ do {
 			    		printf("%02d",studentlist[count].birthday.month);
 			    		printf("-");
 			    		printf("%d",studentlist[count].birthday.year);
-			    		printf(" %.4f",studentlist[count].gpa);	
+			    		printf(" %.4f",studentlist[count].gpa);
 					printf("\n");
 			    		}
-					}		
+					}
 		    	break;
 			 }
 
      	 case 4:	{
-		        /*Write student list to txt*/	
+		        /*Write student list to txt*/
 				fPointer = fopen("database.txt", "w");
                 for (count=0;count<arraycount;count++)	{
-                 
-                	fprintf(fPointer,"%-10.10s %d \t %d \t %d \t %.4f \n", 
-                           studentlist[count].name, 
-                           studentlist[count].birthday.day, 
+
+                	fprintf(fPointer,"%-10.10s %d \t %d \t %d \t %.4f \n",
+                           studentlist[count].name,
+                           studentlist[count].birthday.day,
                            studentlist[count].birthday.month,
                            studentlist[count].birthday.year,
-                           studentlist[count].gpa);                                
+                           studentlist[count].gpa);
 		    	    }
 		    	    fclose(fPointer);
-		
-		    	 break;	
+
+		    	 break;
 			   }
 
-     	    	
+
      	 case 5:
 
 		     	 /* Read student list from db */
 		    	 fPointer = fopen("database.txt", "r");
 		    	    arraycount=0;
-		    	    if (fPointer!=NULL)	{	
+		    	    if (fPointer!=NULL)	{
 				   do	{
-			     fscanf(fPointer,"%s %d %d %d %f\n", 
-                        studentlist[arraycount].name, 
-                        &studentlist[arraycount].birthday.day, 
-                        &studentlist[arraycount].birthday.month, 
-                        &studentlist[arraycount].birthday.year, 
+			     fscanf(fPointer,"%s %d %d %d %f\n",
+                        studentlist[arraycount].name,
+                        &studentlist[arraycount].birthday.day,
+                        &studentlist[arraycount].birthday.month,
+                        &studentlist[arraycount].birthday.year,
                         &studentlist[arraycount].gpa);
 
 				    	arraycount++;
@@ -338,21 +338,21 @@ do {
 		   		}
 		   		fclose(fPointer);
 		    	        break;
-		    	
+
 		 case 6:
 
 		   		/*exiting the program*/
 		   		exit(0);
 		  		break;
-		  		
+
 		   default:
 
 		    /*for any value outside the menu - careless users*/
 		    printf("Invalid choice.\n");
-		    	 break;	
-		    		
+		    	 break;
+
 		        }
-	     } 
+	     }
                while(Menuchoice!=6);
 
 
