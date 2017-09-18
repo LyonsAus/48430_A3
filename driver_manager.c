fcf867b160868c8fe4061cb72e90ffda13e7347f
LyonsAus <alexlyons12@gmail.com>
2017-09-18 11:42:40 +1000

    Added Menu

--- driver_manager.c
+++ driver_manager.c
@@ -1,12 +1,16 @@
 /*
+
+test comment for github desktop
+
+*/
+
+/*
 Group 10:
 Alex Lyons 99126827
 Godwin Huang 12458004
 Hasan Rahman 12457988
 Minh Van 11104332
 Omar Salam 12002376
-
-Driver Management System
 */
 
 #include <stdio.h>
@@ -18,6 +22,9 @@ Driver Management System
 Definiteions for limiting array sizes
 */
 
+
+/**** TEST COMMMENT ****/
+
 #define MAX_NUM_DRIVERS 50
 #define MAX_NAME_SIZE 11
 
@@ -25,7 +32,7 @@ Definiteions for limiting array sizes
 driver struct:
 
 Address -
-post code is limited to 2000-2999 for NSW codes
+post code is limited to 1000-2999 for NSW codes
 */
 
 struct driver{
@@ -57,14 +64,18 @@ struct licence{
 /*
 Functions:
 
-Add Driver - Add a driver to the list. The list must be in a decrypted state to add a driver.
+Add Driver - Add a driver to the list. The list must be in a decrypted state to
+            add a driver.
 Delete Driver - Delete that last driver entered.
 Delete List - Delete all entered drivers.
 Print List - Print the list of drivers (encrypted or decrypted).
-Search List - Display results based on a search term. Limited to search by licence type.
-Encrypt List - After drivers are added the list is encrypted and a decryption passcode is set.
+Search List - Display results based on a search term. Limited to search by
+              licence type.
+Encrypt List - After drivers are added the list is encrypted and a decryption
+              passcode is set.
 Compress List - Compress the list of drivers.
-Decrypt List - The program requests a decryption passcode and decrypts the list upon authentication.
+Decrypt List - The program requests a decryption passcode and decrypts the
+              list upon authentication.
 Decompress List - Decompress the list of drivers after compression.
 */
 
@@ -79,73 +90,107 @@ void compressList();
 void decryptList();
 void decompressList();
 
-
 int main(void) {
-  /* code */
-  return 0;
+  /*
+  menu:
+    The menu uses a switch/break to select menu options. The menu choice
+    integer is initialised at 0.
+  */
+  int menuChoice = 0;
+  do{
+    printMenu();
+    if(scanf("%d", &menuChoice) <=0){
+      printf("Invalid Choice");
+    }
+    else{
+      switch (menuChoice){
+        case 1:
+          addDriver();
+          break;
+        case 2:
+          deleteDriver();
+          break;
+        case 3:
+          deleteList();
+          break;
+        case 4:
+          printList();
+          break;
+        case 5:
+          searchList();
+          break;
+        case 6:
+          encryptList();
+          break;
+        case 7:
+          compressList();
+          break;
+        case 8:
+          decryptList();
+          break;
+        case 9:
+          decompressList();
+          break;
+        case 10:
+          exit(0);
+        default:
+          printf("Invalid Choice");
+          break;
+      }
+    }
+  }
+  while(menuChoice != 10);
+
+  return 1;
 }
 
-  /*
-  Function work allocation is prelimiary/in draft; this will be iterated once
-  the amount of work required for each funciton is more clear.
-  */
-
 void printMenu(){
-  /*
-  Alex Lyons
-  */
+  printf("\n\n"
+    "1. Add Driver\n"
+    "2. Delete Last Driver\n"
+    "3. Delete Driver List\n"
+    "4. Print Driver List\n"
+    "5. Search Driver List\n"
+    "6. Encrypt Driver List\n"
+    "7. Compress Driver List\n"
+    "8. Decrypt Driver List\n"
+    "9. Decompress Driver List\n"
+    "10. Exit\n"
+    "Enter Your Choice> ");
 }
 
 void addDriver(){
-  /*
-  Alex Lyons
-  */
+
 }
 
 void deleteDriver(){
-  /*
-  Alex Lyons
-  */
+
 }
 
 void deleteList(){
-  /*
-  Alex Lyons
-  */
+
 }
 
 void printList(){
-  /*
-  Alex Lyons
-  */
+
 }
 
 void searchList(){
-  /*
-  Alex Lyons
-  */
+
 }
 
 void encryptList(){
-  /*
-  Omar Salam
-  */
+
 }
 
 void compressList(){
-  /*
-  Minh Van
-  */
+
 }
 
 void decryptList(){
-  /*
-  Hasan Rahman
-  */
+
 }
 
 void decompressList(){
-  /*
-  Godwin Huang
-  */
+
 }
